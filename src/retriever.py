fec43d49df1d77d4fbc85233d7fd243f14c2fbbe
sital-tharu <sitatharu769@gmail.com>
2026-06-29 18:55:09 +0530

    create a chromaDB cient

--- src/retriever.py
+++ src/retriever.py
@@ -6,3 +6,15 @@ import dspy
 CHROMA_DB_PATH  = "chroma_db"
 COLLECTION_NAME = "novadesk_kb"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+
+# ── ChromaDB Client ─────────────────────────────────────────────────
+def get_collection():
+    embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(
+        model_name=EMBEDDING_MODEL
+    )
+    client = chromadb.PersistentClient(path=CHROMA_DB_PATH)
+    collection = client.get_collection(
+        name=COLLECTION_NAME,
+        embedding_function=embedding_fn
+    )
+    return collection
